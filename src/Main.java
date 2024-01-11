ceb4adc3ab41aa5511f28ffe61247bc6ebe28df8
= <=>
2024-01-11 16:56:11 +0600

    новый комментарий коммита 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,11 @@
-
-
 public class Main {
     public static void main(String[] args) {
+        System.out.println("MODIFIED");
 
+
+        // feature: добавлен аутентификатор Google
+        // fix: исправлен баг возврат значение контроллера
+        // refactoring: декомпозирован класс Manager.java
+        // style: ...
     }
 }
